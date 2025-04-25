b083e996cdf1640b8ed1c5cd1bb5c59d64212f9c
TranHieu305 <trunghieu2000.tdn@gmail.com>
2025-04-26 04:56:37 +0700

    Feat: Initial Catalog service

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProduct.Handler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, string Description, string ImageFile, decimal Price, List<string> Category) : IRequest<CreateProductResponse>;
+    public record class CreateProductResponse(Guid Id);
+    public class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResponse>
+    {
+        public Task<CreateProductResponse> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            return null;
+        }
+    }
+}
