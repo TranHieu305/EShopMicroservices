b6074ac6e5ab3a97882bce45f3fccc9ec1a8d24c
TranHieu305 <trunghieu2000.tdn@gmail.com>
2025-04-29 06:04:05 +0700

    Feat: building blocks prj

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProduct.Handler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProduct.Handler.cs
@@ -1,14 +1,31 @@
-﻿using MediatR;
+﻿
+using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
 namespace Catalog.API.Products.CreateProduct
 {
-    public record CreateProductCommand(string Name, string Description, string ImageFile, decimal Price, List<string> Category) : IRequest<CreateProductResponse>;
+    public record CreateProductCommand(string Name, string Description, string ImageFile, decimal Price, List<string> Category) 
+        : ICommand<CreateProductResponse>;
     public record class CreateProductResponse(Guid Id);
-    public class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResponse>
+    public class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResponse>
     {
-        public Task<CreateProductResponse> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResponse> Handle(CreateProductCommand request, CancellationToken cancellationToken)
         {
-            return null;
+            // Create product entity
+            var newProduct = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Description = request.Description,
+                ImageFile = request.ImageFile,
+                Price = request.Price,
+                Category = request.Category
+            };
+
+            // Save to db
+
+            // Return the created product ID
+            return new CreateProductResponse(newProduct.Id);
         }
     }
 }
